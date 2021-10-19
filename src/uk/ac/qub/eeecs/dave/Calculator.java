0c6bd53fdc8625ca55cbc6a0beb07f99b9eb2e6f
Ryan Johnston <rjohnston798@icloud.com>
2021-10-19 14:13:02 +0100

    Update

--- src/uk/ac/qub/eeecs/dave/Calculator.java
+++ src/uk/ac/qub/eeecs/dave/Calculator.java
@@ -1,8 +1,8 @@
 package uk.ac.qub.eeecs.dave;
 
 public class Calculator {
-	public int add(int a, int b) {
-		return a+b;
+	public int add(int a, int b, int c) {
+		return a+b+c;
 	}
 	
 	public int sub(int a, int b) {
